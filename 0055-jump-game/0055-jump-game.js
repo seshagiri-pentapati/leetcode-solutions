42c8386e9242b04fa294798e925e5e2d28a185d7
Seshagiri Pentapati <88339880+seshagiri-pentapati@users.noreply.github.com>
2026-08-06 20:52:38 +0530

    Time: 3 ms (35.8%), Space: 58.8 MB (76.34%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let n = nums.length;
+    let maxReach = 0;
+    for(let i=0; i<n; i++) {
+        if(i > maxReach) {
+            return false;
+        }
+        maxReach = Math.max(maxReach, i+ nums[i]);
+        if(maxReach >= n-1) {
+            return true;
+        }
+    }
+};
