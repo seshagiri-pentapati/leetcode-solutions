a7e79101a856cfbb44735c6fd5eb2cd0593ef730
Seshagiri Pentapati <88339880+seshagiri-pentapati@users.noreply.github.com>
2026-08-11 23:32:23 +0530

    Time: 58 ms (82.35%), Space: 56.3 MB (94.12%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function (strs, m, n) {
+    
+    const dp = Array.from({ length: m + 1 }, () =>
+        Array(n + 1).fill(0)
+    );
+
+    for (const str of strs) {
+        let zeros = 0;
+        let ones = 0;
+
+        for (const ch of str) {
+            if (ch === "0") {
+                zeros++;
+            } else {
+                ones++;
+            }
+        }
+
+        for (let i = m; i >= zeros; i--) {
+            for (let j = n; j >= ones; j--) {
+                dp[i][j] = Math.max(
+                    dp[i][j],
+                    dp[i - zeros][j - ones] + 1
+                );
+            }
+        }
+    }
+
+    return dp[m][n];
+};
